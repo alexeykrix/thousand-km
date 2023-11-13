81169e978eba2e55dcd7fc7dfc687ac68af8b80d
Alexey Krix <alexey@thefirstthelast.agency>
2023-11-13 19:51:40 +0200

    init

---
+++ android/src/com/krix/thousand/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.krix.thousand;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.krix.thousand.Thousand;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Thousand(), config);
+	}
+}
---
+++ ios/src/com/krix/thousand/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.krix.thousand;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.krix.thousand.Thousand;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Thousand(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
